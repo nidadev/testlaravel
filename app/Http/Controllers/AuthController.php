0cfdb3db3734a900c6473e579695ffe333d50a2d
nidadev <53574300+nidadev@users.noreply.github.com>
2021-06-20 17:13:33 +0500

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -57,4 +57,13 @@ class AuthController extends Controller
     {
         return $this->respondWithToken(auth()->refresh());
     }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
 }
